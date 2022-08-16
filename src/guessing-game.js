15e9f4f056bfdf0e28413a132de32909205fb3bc
taoyami <taoyami@yandex.ru>
2022-08-16 22:02:01 +0300

    feat: done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,22 @@
 class GuessingGame {
     constructor() {}
-
+  
     setRange(min, max) {
-
+      this.max = max;
+      this.min = min;
     }
-
+  
     guess() {
-
+      return Math.round((this.max + this.min) / 2);
     }
-
+  
     lower() {
-
+      this.max = this.guess();
     }
-
+  
     greater() {
-
+      this.min = this.guess();
     }
-}
-
-module.exports = GuessingGame;
+  }
+  
+  module.exports = GuessingGame;
